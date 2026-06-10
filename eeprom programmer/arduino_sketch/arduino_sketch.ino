5a2bb94f844e31c15f1321eebdbbf70f1ce5df17
SrivatsaRUpadhya <srivatsarupadhya@gmail.com>
2026-06-11 00:30:27 +0530

    fix: incorrect address being sent to shift register

--- eeprom programmer/arduino_sketch/arduino_sketch.ino
+++ eeprom programmer/arduino_sketch/arduino_sketch.ino
@@ -10,12 +10,15 @@
 #define PACKET_SIZE 64
 #define OP_MASK 0x80
 #define WRITE_MODE_MASK 0x40
-#define BYTE_COUNT_MASK 0x3E
+#define BYTE_COUNT_MASK 0x7E
 #define OP_WRITE 0x80
 #define OP_READ 0x00
 #define DATA_OFFSET 3
+#define RES_DATA_OFFSET 1
+#define RES_LEN_OFFSET 0
+#define RES_PACKET_SIZE 63
 
-int data_pins[] = { 6, 7, 8, 9, 10, 11, 12, 13 };
+byte data_pins[] = { 6, 7, 8, 9, 10, 11, 12, 13 };
 
 void op_unknown(int op) {
   Serial.print("unknow operation: ");
@@ -34,23 +37,22 @@ short getAddr(byte *buf) {
 }
 void setAddress(short addr) {
   // send first byte MSBFIRST
-  shiftOut(SDA, SRCLK, MSBFIRST, addr & 0xF0);
+  shiftOut(SDA, SRCLK, MSBFIRST, addr & 0xFF00);
   // send second byte MSBFIRST
-  shiftOut(SDA, SRCLK, MSBFIRST, addr & 0x0F);
+  shiftOut(SDA, SRCLK, MSBFIRST, addr & 0x00FF);
 
   // Pulse the Register clock to latch in the address to storage register
   digitalWrite(RCLK, HIGH);
   digitalWrite(RCLK, LOW);
-
+  delay(20);
   // TODO: remove debug statements
-  Serial.print("address set to: ");
-  Serial.flush();
-  Serial.println(addr);
-  Serial.flush();
+  // Serial.print("address set to: ");
+  // Serial.flush();
+  // Serial.println(addr);
+  // Serial.flush();
 }
 
 void writeData(byte *buf, short addr, byte bytes_count) {
-  Serial.println(bytes_count);
   for (byte idx = 0; idx < bytes_count; idx++) {
     setAddress(addr++);
     writeToAddress(buf[DATA_OFFSET + idx]);
@@ -58,6 +60,7 @@ void writeData(byte *buf, short addr, byte bytes_count) {
 }
 
 void writeToAddress(byte data) {
+  digitalWrite(OE, HIGH);
   setDataMode(OUTPUT);
   // set the data on the data pins
   for (int i = 7; i >= 0; i--) {
@@ -66,34 +69,47 @@ void writeToAddress(byte data) {
   }
 
   // Disable output
-  digitalWrite(OE, HIGH);
   // Pulse write enable to write the data
   digitalWrite(WE, LOW);
+  delayMicroseconds(10);
   digitalWrite(WE, HIGH);
 }
 
-void readFromAddress() {
+void readData(short startAddr, byte bytesCount) {
+  byte buf[RES_PACKET_SIZE];
+  memset(buf, 0, RES_PACKET_SIZE);
+  byte i = 0;
+  for (i = 0; i < bytesCount; i++) {
+    setAddress(startAddr++);
+    buf[RES_DATA_OFFSET + i] = readFromAddress();
+  }
+  buf[RES_LEN_OFFSET] = i + 1;
+
+  Serial.write(buf, RES_PACKET_SIZE);
+  Serial.flush();
+}
+
+byte readFromAddress() {
   // Disable write and enable output from EEPROM
-  digitalWrite(OE, LOW);
   digitalWrite(WE, HIGH);
+  digitalWrite(OE, LOW);
 
   setDataMode(INPUT);
-  int res = 0;
+  byte res = 0;
   // Read in the byte
-  for (int i = 0; i < DATA_SIZE; i++) {
+  for (byte i = 0; i < DATA_SIZE; i++) {
     int b = digitalRead(data_pins[i]);
     res = (res << 1);
-    res ^= b;
+    res |= b;
   }
 
-  Serial.print("Data: ");
-  Serial.println(res);
-  Serial.flush();
-  digitalWrite(OE, HIGH);
+  // Serial.print("Data: ");
+  // Serial.println(res);
+  // Serial.flush();
+  return res;
 }
 
 void readPacket(byte *buf) {
-read:
   while (Serial.available() == 0)
     ;
   int bytes_read = Serial.readBytes(buf, PACKET_SIZE);
@@ -107,7 +123,7 @@ void setup() {
   pinMode(SDA, OUTPUT);
 
   digitalWrite(RCLK, LOW);
-  digitalWrite(OE, LOW);
+  digitalWrite(OE, HIGH);
   digitalWrite(WE, HIGH);
 
   Serial.begin(9600);
@@ -120,20 +136,20 @@ void loop() {
 
   byte header = buf[0];
   byte op = header & OP_MASK;                               // first bit of header indicates operation
-  byte write_mode = header & WRITE_MODE_MASK;               // second bit of header indicates byte write mode
   byte data_bytes_count = (header & BYTE_COUNT_MASK) >> 1;  // number of bytes to write
 
   short addr = getAddr(buf);
+  delay(10);
 
   switch (op) {
     case OP_READ:
-      setAddress(addr);
-      readFromAddress();
+      readData(addr, data_bytes_count);
       break;
     case OP_WRITE:
       writeData(buf, addr, data_bytes_count);
       break;
     default:
       op_unknown(op);
+      break;
   }
 }
--- eeprom programmer/flasher.c
+++ eeprom programmer/flasher.c
@@ -12,29 +12,51 @@
 #include <termios.h>
 #include <unistd.h>
 
-#define USB_SERIAL "/dev/ttyACM1"
+#define USB_SERIAL "/dev/ttyACM0"
 #define DATA_FILE "./data"
 #define BYTES_PER_LINE 8
 #define MAX_DATA_FILE_SIZE 32768
+#define CMD_LEN 64
+#define DATA_OFFSET 3
+#define OP_WRITE 0x80
+#define OP_READ 0x00
+#define MAX_BYTES_PER_READ 62
+#define MAX_BYTES_PER_WRITE 61
+#define ADDRESS_OFFSET 1
+#define ADDRESS_SIZE 2
+#define RES_PACKET_SIZE 63
+#define RES_DATA_OFFSET 1
+#define EEPROM_SIZE 32767
 
 void dump_buf(uint8_t *buf, int len);
 void print_text(char *buf);
+void write_data(short addr, FILE *data_fp, int port_fd, int data_file_size);
+void dump_eeprom(int port_fd, short addr);
+
 int config_termios(int port_fd) {
   struct termios tty;
-  if (tcgetattr(port_fd, &tty) != 0) {
-    perror("Error getting serial attributes");
+  if (tcgetattr(port_fd, &tty) < 0) {
+    printf("error getting termios config");
     return -1;
   }
+  struct termios config;
 
-  // Set Baud Rate to 9600
-  cfsetospeed(&tty, B9600);
-  cfsetispeed(&tty, B9600);
-  if (tcsetattr(port_fd, TCSANOW, &tty) != 0) {
-    perror("Error setting serial attributes");
-    return -1;
-  }
+  cfmakeraw(&config);
+  config.c_cflag |= (CLOCAL | CREAD);
+  config.c_iflag &= ~(IXOFF | IXANY);
+
+  // set vtime, vmin, baud rate...
+  config.c_cc[VMIN] = RES_PACKET_SIZE; // you likely don't want to change this
+  config.c_cc[VTIME] = 0;              // or this
+
+  cfsetispeed(&config, B9600);
+  cfsetospeed(&config, B9600);
+
+  // write port configuration to driver
+  tcsetattr(port_fd, TCSANOW, &config);
   return 0;
 }
+
 int validate_data_file(FILE *data_fp) {
   struct stat st;
   int data_fd = fileno(data_fp);
@@ -43,15 +65,15 @@ int validate_data_file(FILE *data_fp) {
     printf("Data file exceeds %d bytes", MAX_DATA_FILE_SIZE);
     return -1;
   }
-  return 0;
+  return st.st_size;
 }
 
-void dump_buf(uint8_t *buf, int len) {
+void dump_cmd_data(uint8_t *buf, int len) {
   printf("\nbuffer dump:\n");
   int byte_count = 0;
   for (int i = 0; i < len; i++) {
-    printf("%#x ", buf[i]);
-    // printf("%c", buf[i]);
+    // printf("%x ", buf[i]);
+    printf("%c", buf[i]);
     if (byte_count++ == BYTES_PER_LINE) {
       printf("\n");
       byte_count = 0;
@@ -77,7 +99,9 @@ int main() {
     printf("unable to open data file");
     return -1;
   }
-  if (validate_data_file(data_fp) == -1) {
+
+  int data_file_size = validate_data_file(data_fp);
+  if (data_file_size == -1) {
     printf("data file invalid");
   }
 
@@ -87,30 +111,59 @@ int main() {
 
   // setup read buffer
   uint8_t cmd[64];
-  memset(cmd, 0, sizeof(cmd));
-  cmd[0] = 0x80;
-  int addr = 0;
-  uint8_t bytes_count = 2;
-  bytes_count = bytes_count << 1;
-  cmd[0] |= bytes_count;
-  memcpy(cmd + 1, &addr, 2);
-  cmd[3] = 'A';
-  cmd[4] = 'B';
-  dump_buf(cmd, sizeof(cmd));
-  write(port_fd, cmd, sizeof(cmd));
-
-  // read back
-  usleep(500);
-  cmd[0] = 0x00;
-  memcpy(cmd + 1, &addr, 2);
-  dump_buf(cmd, sizeof(cmd));
-  write(port_fd, cmd, sizeof(cmd));
-
-  addr = 1;
-  cmd[0] = 0x00;
-  memcpy(cmd + 1, &addr, 2);
-  dump_buf(cmd, sizeof(cmd));
-  write(port_fd, cmd, sizeof(cmd));
+  short addr = 0;
+  write_data(addr, data_fp, port_fd, data_file_size);
+  sleep(1);
+  printf("\n==========================\n");
+  dump_eeprom(port_fd, addr);
 
   close(port_fd);
+}
+
+void write_data(short addr, FILE *data_fp, int port_fd, int data_file_size) {
+  uint8_t cmd[CMD_LEN];
+
+  while (data_file_size != 0) {
+    sleep(3);
+    memset(cmd, 0, sizeof(cmd));
+    int n =
+        fread(cmd + DATA_OFFSET, sizeof(uint8_t), MAX_BYTES_PER_WRITE, data_fp);
+    cmd[0] = OP_WRITE | (uint8_t)(n << 1);
+    memcpy(cmd + ADDRESS_OFFSET, &addr, ADDRESS_SIZE);
+    dump_cmd_data(cmd, sizeof(cmd));
+    write(port_fd, cmd, sizeof(cmd));
+    fsync(port_fd);
+    data_file_size -= n;
+    addr += n;
+  }
+}
+
+void read_packet(int port_fd, uint8_t buf[]) {
+  int bytes_read = 0;
+  while (bytes_read < RES_PACKET_SIZE) {
+    bytes_read += read(port_fd, buf + bytes_read, RES_PACKET_SIZE);
+    printf("%d\n", bytes_read);
+  }
+}
+
+void dump_eeprom(int port_fd, short addr) {
+  u_short bytes_read = 0;
+  uint8_t cmd[CMD_LEN];
+
+  while (1) {
+    printf("start addr: %d\n", addr);
+    memset(cmd, 0, sizeof(cmd));
+    cmd[0] = OP_READ | ((uint8_t)MAX_BYTES_PER_READ << 1);
+    memcpy(cmd + ADDRESS_OFFSET, &addr, ADDRESS_SIZE);
+    write(port_fd, cmd, sizeof(cmd));
+    fsync(port_fd);
+    read(port_fd, cmd, RES_PACKET_SIZE);
+    printf("bytes read: %d\n", cmd[0]);
+    bytes_read += cmd[0];
+    dump_cmd_data(cmd + RES_DATA_OFFSET, RES_PACKET_SIZE - 1);
+    addr += bytes_read;
+    if (bytes_read == EEPROM_SIZE) {
+      break;
+    }
+  }
 }
